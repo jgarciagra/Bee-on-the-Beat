c5bb8739115a16a5e8423c72642f4228fd9eebdb
egutierrezm <egutierrezm@edu.tecnocampus.cat>
2025-05-31 11:17:05 +0200

    level menu

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+    public void GoToLevelOne()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void GoToLevelTwo()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void GoToLevelThree()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+}
