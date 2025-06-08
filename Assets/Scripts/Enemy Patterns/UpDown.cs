e8f87a706387d3f5eaf0e915780c4c71d91be3d8
jgarciagra <jgarciagra@edu.tecnocampus.cat>
2025-06-08 23:27:46 +0200

    commit

---
+++ Assets/Scripts/Enemy Patterns/UpDown.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Patterns/UpDown")]
+public class UpDown : EnemyPattern
+{
+    
+    public override Vector2Int GetDirection(EnemyContoller enemy)
+    {
+        if (!enemy.hasPatternInitialized)
+        {
+            enemy.customState = enemy.initialUp;
+            enemy.hasPatternInitialized = true;
+        }
+        Vector2Int dir;
+
+        if (enemy.customState)
+        {
+            dir = Vector2Int.up;
+        }
+        else
+        {
+            dir = Vector2Int.down;
+        }
+
+        enemy.customState = !enemy.customState;
+        return dir;
+    }
+
+}
